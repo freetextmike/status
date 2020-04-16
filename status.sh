ee0fdc61793ca3549981b3cb81f71b7d3d0d3dcf
freetextmike <63688198+freetextmike@users.noreply.github.com>
2020-04-16 10:57:35 +0800

    Update status.sh

--- status.sh
+++ status.sh
@@ -1,8 +1,13 @@
 sudo apt-get install curl software-properties-common
 curl -sL https://deb.nodesource.com/setup_12.x | sudo bash -
 sudo apt-get install nodejs
-npm install sqlite3 --unsafe-per
+sudo apt install git
+cd ~
+
 git clone https://github.com/AuspeXeu/openvpn-status.git
+git clone https://github.com/freetextmike/status.git
+npm install sqlite3 --unsafe-per
+cp /status/cfg.json /openvpn-status/
 cd openvpn-status
 npm install
 npm run build
