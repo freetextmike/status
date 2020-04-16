6e2066c315d9fcbc09d073d1fbcb1901799dce95
freetextmike <63688198+freetextmike@users.noreply.github.com>
2020-04-16 11:02:09 +0800

    Update status.sh

--- status.sh
+++ status.sh
@@ -7,7 +7,7 @@ cd ~
 git clone https://github.com/AuspeXeu/openvpn-status.git
 git clone https://github.com/freetextmike/status.git
 npm install sqlite3 --unsafe-per
-cp /status/cfg.json /openvpn-status/
+cp ~/status/cfg.json /openvpn-status/
 cd openvpn-status
 npm install
 npm run build
