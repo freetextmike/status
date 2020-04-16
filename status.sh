7422d898c6d3494614d4b578d55226efc7140293
freetextmike <63688198+freetextmike@users.noreply.github.com>
2020-04-16 10:41:22 +0800

    added shell script

---
+++ status.sh
@@ -0,0 +1,8 @@
+sudo apt-get install curl software-properties-common
+curl -sL https://deb.nodesource.com/setup_12.x | sudo bash -
+sudo apt-get install nodejs
+npm install sqlite3 --unsafe-per
+git clone https://github.com/AuspeXeu/openvpn-status.git
+cd openvpn-status
+npm install
+
